50dc997c92736b28fd251446c88af962b054cdbc
Michael Straka <mstraka100@gmail.com>
2022-04-29 17:20:20 -0500

    gen random

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use ark_ec::short_weierstrass_jacobian::GroupProjective;
+use ark_ec::msm::VariableBaseMSM::multi_scalar_mul;
 use ark_ec::models::SWModelParameters as Parameters;
+use ark_bls12_377 as bls377;
+use ark_bls12_377::G1Projective;
+use ark_ff::fields::Field;
+use ark_std::Zero;
+use ark_serialize::CanonicalSerialize;
 use rand::RngCore;
 //use rand::prelude::Distribution;
 use ark_std::rand::{
@@ -11,11 +17,21 @@ fn main() {
     println!("Hello, world!");
 }
 
-pub fn random_multi_scalar_mul<P: Parameters, R: RngCore>(
+pub fn random_multi_scalar_mul<R: RngCore>(
     rng: &mut R,
 ) -> () {
     let n = 5;
-    let point: GroupProjective<P> = rng.gen();
-    //let standard = Standard::new();
-    //let point = D::sample(&standard, rng);
+    let num_bytes = bls377::Fr::zero().serialized_size();
+    let mut points = Vec::<G1Projective>::new();
+    let mut scalars = Vec::<bls377::Fr>::new();
+    for i in 0..n {
+        let mut point: G1Projective = rng.gen();
+        points.push(point);
+
+        let mut bytes = vec![0; num_bytes];
+        rng.fill_bytes(&mut bytes[..]);
+        let scalar = bls377::Fr::from_random_bytes(&bytes).unwrap();
+        scalars.push(scalar);
+    }
+    let result = multi_scalar_mul(&points[..], &scalars[..]);
 }
