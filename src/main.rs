b3fb8d305b699414fdb52c7d2d9969b270f4d7f5
Michael Straka <mstraka100@gmail.com>
2022-05-02 11:27:43 -0500

    single iteration benchmark

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use ark_ec::short_weierstrass_jacobian::GroupProjective;
-use ark_ec::msm::VariableBaseMSM::multi_scalar_mul;
+use ark_ff::PrimeField;
+use std::time::Instant;
+use ark_ec::msm;
 use ark_ec::models::SWModelParameters as Parameters;
 use ark_bls12_377 as bls377;
-use ark_bls12_377::G1Projective;
+use ark_bls12_377::{G1Affine, G1Projective};
 use ark_ff::fields::Field;
 use ark_std::Zero;
 use ark_serialize::CanonicalSerialize;
 use rand::RngCore;
+use rand::thread_rng;
 //use rand::prelude::Distribution;
 use ark_std::rand::{
     distributions::{Distribution, Standard},
@@ -14,24 +17,36 @@ use ark_std::rand::{
 };
 
 fn main() {
-    println!("Hello, world!");
+    let mut rng = thread_rng();
+    random_multi_scalar_mul(100, 5, &mut rng);
 }
 
 pub fn random_multi_scalar_mul<R: RngCore>(
+    n: usize,
+    iterations: usize,
     rng: &mut R,
 ) -> () {
-    let n = 5;
     let num_bytes = bls377::Fr::zero().serialized_size();
-    let mut points = Vec::<G1Projective>::new();
-    let mut scalars = Vec::<bls377::Fr>::new();
+    let mut points = Vec::<G1Affine>::new();
+    let mut scalars = Vec::<<bls377::Fr as PrimeField>::BigInt>::new();
     for i in 0..n {
-        let mut point: G1Projective = rng.gen();
-        points.push(point);
-
         let mut bytes = vec![0; num_bytes];
-        rng.fill_bytes(&mut bytes[..]);
-        let scalar = bls377::Fr::from_random_bytes(&bytes).unwrap();
-        scalars.push(scalar);
+        let mut scalar;
+        loop {
+            rng.fill_bytes(&mut bytes[..]);
+            scalar = bls377::Fr::from_random_bytes(&bytes);
+            if scalar.is_some() {
+                break;
+            }
+        }
+        scalars.push(scalar.unwrap().into_repr());
+
+        let mut point: G1Projective = rng.gen();
+        points.push(point.into());
     }
-    let result = multi_scalar_mul(&points[..], &scalars[..]);
+    let start = Instant::now();
+    let result = ark_ec::msm::VariableBaseMSM::multi_scalar_mul(&points[..], &scalars[..]);
+    let duration = start.elapsed();
+
+    println!("Time to execute MSM is: {:?}", duration);
 }
