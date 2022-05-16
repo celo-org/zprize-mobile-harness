502daa028683fbfbfd0f92eecd76612551f1c03f
Michael Straka <mstraka100@gmail.com>
2022-05-16 11:31:54 -0500

    button

--- android/ZPrize/app/src/main/java/com/example/zprize/MainActivity.java
+++ android/ZPrize/app/src/main/java/com/example/zprize/MainActivity.java
@@ -5,19 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.io.File;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        RustMSM g = new RustMSM();
-        File dir = getFilesDir();
-        String dir_path = dir.getAbsolutePath();
-        String r = g.runMSM(dir_path);
+
+
+       /* super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ((TextView)findViewById(R.id.greetingField)).setText("hello charlie");*/
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ((TextView)findViewById(R.id.greetingField)).setText("hello charlie");
+
+        LinearLayout linearLayout = findViewById(R.id.rootContainer);
+
+        // Create Button Dynamically
+        Button btnShow = new Button(this);
+        btnShow.setText("Press to run");
+        btnShow.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RustMSM g = new RustMSM();
+                File dir = getFilesDir();
+                String dir_path = dir.getAbsolutePath();
+                String r = g.runMSM(dir_path);
+                Toast.makeText(MainActivity.this, "welcome alpha!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Add Button to LinearLayout
+        if (linearLayout != null) {
+            linearLayout.addView(btnShow);
+        }
+
     }
 
     static {
